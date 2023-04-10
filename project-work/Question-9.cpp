0350522584f007d5c200f4a09a499704e5e4a9f6
AASTU-Group-4 <130121277+AASTU-Group-4@users.noreply.github.com>
2023-04-10 15:16:12 +0300

    correcting an error

--- project-work/Question-9.cpp
+++ project-work/Question-9.cpp
@@ -8,11 +8,11 @@ using namespace std;
 
 int main() 
 {
-    int num;
-    bool is_prime = true;
+    int num; // Declare an integer variable num
+    bool is_prime = true; // Declare a boolean variable is_prime and initialize it to true
 
-    cout << "Enter an integer: ";
-    cin >> num;
+    cout << "Enter an integer: "; // Prompt the user to enter an integer
+    cin >> num; // Read the integer from the user
 
     // Check if num is less than 3
     if (num < 3) 
@@ -41,22 +41,28 @@ int main()
         }
         if (is_prime) // If num is a prime number
         {
-            double sum = 0;
-            int count = 0;
+            double sum = 0; // Declare a double variable sum and initialize it to 0
+            int count = 0; // Declare an integer variable count and initialize it to 0
             // Loop through all the numbers from 2 to num-1 to calculate the average value of prime numbers
-            for (int i = 2; i < num-1; i++) 
+            for (int i = 2; i < num; i++) 
             {
+                bool prime = true; // Declare a boolean variable prime and initialize it to true
                 // Check if i is a prime number by looping through all the numbers from 2 to sqrt(i)
                 for (int j = 2; j <= sqrt(i); j++) 
                 {
                     // If i is divisible by j, then it's not a prime number
                     if (i % j == 0) 
                     {
-                        i++; // Skip to the next number
+                        prime = false;
+                        break;
                     }
+                    
                 }
-                sum += i; // Add i to the sum
-                count++; // Increment the count
+                if(prime)
+                {
+                    sum += i; // Add i to the sum
+                    count++; // Increment the count
+                }
             }
             double average = sum / count; // Calculate the average value
             cout << num << " is a prime number with an average value of prime numbers between 1 and "; 
@@ -79,6 +85,3 @@ int main()
             }
         }
     }
-
-    return 0;
-}
